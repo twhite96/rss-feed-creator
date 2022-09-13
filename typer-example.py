db80e8e5e6b594c2cb260360519b83b4ec8fd421
Tiffany <hey@tiffanyrwhite.com>
2022-09-13 16:40:44 -0400

    Remove nonsense from docs

---
+++ typer-example.py
@@ -0,0 +1,23 @@
+import typer
+from enum import Enum
+from typing import List, Optional
+
+class ApprovedVaccine(str, Enum):
+    pfizer = "pfizer"
+    moderna = "moderna"
+    astrazeneca = "astrazeneca"
+    janssen = "janssen"
+    sinovac = "sinovac"
+
+def main(
+    name: str = typer.Option(..., help="Your Name"),
+    birth: str = typer.Option(..., help="Your birthday in YYYY-MM-DD format", callback=validate_date),
+    manufacturer: Optional[List[ApprovedVaccine]] = typer.Option(..., help="The vaccine manufacturer", case_sensitive=False),
+    date: Optional[List[str]] = typer.Option(..., help="The date of vaccination", callback=validate_date)
+):
+    """Generate your vaccination QR code."""
+    # same logic as Click example
+    print(name, birth, manufacturer, date)
+
+if __name__ == "__main__":
+    typer.run(main)n
